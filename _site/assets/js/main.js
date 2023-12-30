18560a4ca548a79a2c8c7eb819967a54f34b07c2
Johannes Müller <Johannes.Mueller@exxeta.com>
2023-12-30 17:13:22 +0100

    Migrate to jekyll and fill in real values

---
+++ _site/assets/js/main.js
@@ -0,0 +1,22 @@
+// dummy function
+window.addEventListener('load', (event) => {
+  console.log("Page loaded.");
+});
+
+
+// Import the Bootstrap bundle
+//
+// This includes Popper and all of Bootstrap's JS plugins.
+
+import "assets/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
+
+
+//
+// Place any custom JS here
+//
+
+// Create an example popover
+document.querySelectorAll('[data-bs-toggle="popover"]')
+  .forEach(popover => {
+    new bootstrap.Popover(popover)
+  })
